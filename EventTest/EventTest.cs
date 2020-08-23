753595ea5cca1b81f0ba1215716ecda7669c99b3
Ivan Kralj <kraljivan488@gmail.com>
2020-08-23 09:06:31 +0200

    Adding event test

---
+++ EventTest/EventTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventTest
+{
+    public class EventTest
+    {
+        public bool Commited { get; set; }
+    }
+
+    public delegate void EventTestDelegate(EventTest args);
+
+    public class Konekcija
+    {
+        public event EventTestDelegate TransactionCompleted;
+
+        public void StartEvent()
+        {
+            TransactionCompleted(new EventTest { Commited = true });
+        }
+    }
+}
---
+++ EventTest/Home.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EventTest
+{
+    class Home
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Konekcija connection = new Konekcija();
+
+            connection.TransactionCompleted += (e) =>
+            {
+                if (!e.Commited)
+                {
+                    return;
+                }
+                Console.WriteLine("Hello World! Event");
+            };
+
+            connection.StartEvent();
+        }
+    }
+}
